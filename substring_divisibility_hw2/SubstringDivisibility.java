fc6a8588b63129be6c71eedf08ed94af34c63f22
brogan20 <brogancle@gmail.com>
2021-02-09 18:55:56 -0600

    Add names and pledge

--- substring_divisibility_hw2/SubstringDivisibility.java
+++ substring_divisibility_hw2/SubstringDivisibility.java
@@ -1,3 +1,6 @@
+// Names: Brogan Clements, Dominick DiMaggio, Ishaan Patel
+// Pledge: I pledge my honor that I have abided by the Stevens Honor System.
+
 public class SubstringDivisibility {
 
     public static void main(String[] args) {
