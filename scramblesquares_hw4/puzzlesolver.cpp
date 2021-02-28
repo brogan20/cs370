fda4ac2aadad48f3a940ae0a4a88002a3656665d
Damino <dom32400@aol.com>
2021-02-27 20:30:53 -0500

    Printing from board

--- scramblesquares_hw4/puzzlesolver.cpp
+++ scramblesquares_hw4/puzzlesolver.cpp
@@ -39,6 +39,47 @@ int is_down(char *buf1, char *buf2) {
     return is_up(buf2, buf1);
 }
 
+int num_from_addr(int lines, char *addr, char (*squares1)[9], char (*squares2)[9], char (*squares3)[9], char (*squares4)[9]) {
+    for(int i = 0; i < lines; i++) {
+        if(squares1[i] == addr || squares2[i] == addr || squares3[i] == addr || squares4[i] == addr)
+            return i+1;
+    }
+
+    return -1;
+}
+
+void print_board(char *board[9], int lines, char (*squares1)[9], char (*squares2)[9], char (*squares3)[9], char (*squares4)[9]) {
+    // Top, LR, Bottom
+    /*
+    std::cout << "*****************************\n";
+    std::cout << num_from_addr(lines, board[3], squares1, squares2, squares3, squares4) << std::endl;
+    for(int i = 0; i < 9; i++) {
+        //std::cout << board[0] << std::endl;
+        printf("%s\n", board[i]);
+    }
+    std::cout << "\n*****************************\n";
+    */
+
+    for(int i = 0; i < 7; i += 3) {
+        std::cout << "+--------+--------+--------+\n";
+        int num1 = num_from_addr(lines, board[i], squares1, squares2, squares3, squares4),
+            num2 = num_from_addr(lines, board[i+1], squares1, squares2, squares3, squares4),
+            num3 = num_from_addr(lines, board[i+2], squares1, squares2, squares3, squares4);
+
+        std::cout << "|" << num1 << "  " << board[i][0] << board[i][1] << "   |" << num2 << "  " << board[i+1][0] << board[i+1][1] << "   |" << num3 << "  " << board[i+2][0] << board[i+2][1] << "   |\n";
+        std::cout << "|" << board[i][6] << board[i][7] << "    " << board[i][2] << board[i][3] << "|" << board[i+1][6] << board[i+1][7] << "    " << board[i+1][2] << board[i+1][3] << "|" << board[i+2][6] << board[i+2][7] << "    " << board[i+2][2] << board[i+2][3] << "|\n";
+        std::cout << "|   " << board[i][4] << board[i][5] << "   |   " << board[i+1][4] << board[i+1][5] << "   |   " << board[i+2][4] << board[i+2][5] << "   |\n";
+    }
+    std::cout << "+--------+--------+--------+\n";
+    
+
+    std::cout << std::endl;
+}
+
+void test(int width, char (*squares)[9]) {
+
+}
+
 int main() {
     FILE *file = fopen("arrows.txt", "r");
     int file_len;
@@ -113,6 +154,26 @@ int main() {
             std::cout << "Letter " << (char)('a' + i) << " has " << pair_counts[i] << " less starts than ends\n";
     }
 
-    char *board[9];
+    std::cout << "Input tiles:\n";
+    for(int i = 0; i < lines; i++) {
+        std::cout << (i+1);
+        std::cout << ". <";
+        for(int j = 0; j < 6; j += 2)
+            std::cout << squares[0][i][j] << squares[0][i][j+1] << ", ";
+        std::cout << squares[0][i][6] << squares[0][i][7] << ">\n";
+    }
 
+    // Array of pointers to squares
+    char *board[9];
+    board[0] = squares[0][0];
+    board[1] = squares[0][1];
+    board[2] = squares[0][2];
+    board[3] = squares[0][3];
+    board[4] = squares[0][4];
+    board[5] = squares[0][5];
+    board[6] = squares[0][6];
+    board[7] = squares[0][7];
+    board[8] = squares[0][8];
+
+    print_board(board, lines, squares[0], squares[1], squares[2], squares[3]);
 }
