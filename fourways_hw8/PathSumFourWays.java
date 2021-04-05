952878ed80590a5fbcb7f04a71e5a823691201f7
Brogan Clements <brogancle@gmail.com>
2021-04-05 18:30:01 -0500

    Fix a problem when multiple weights were the same

--- fourways_hw8/PathSumFourWays.java
+++ fourways_hw8/PathSumFourWays.java
@@ -1,11 +1,39 @@
 import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
 
 public class PathSumFourWays {
-
+    private static class Node implements Comparable<Node>{
+        
+        int weight;
+        
+        int dist = Integer.MAX_VALUE;
+    
+        int i;
+        int j;
+    
+        Node(int weight, int i, int j) {
+            this.weight = weight;
+            this.i = i;
+            this.j = j;
+        }
+    
+        
+        public int compare(Node n1, Node n2) {
+            if (n1.dist == n2.dist) return 0;
+            //returns 1 if greater
+            if (n1.dist > n2.dist) return 1;
+            return -1;
+        } 
+    
+        public int compareTo(Node comp) {
+            if (this.dist == comp.dist) return 0;
+            if (this.dist > comp.dist) return 1;
+            return -1;
+        }
+    
+    }
     public static void main(String[] args) throws Exception{
         File file = new File(args[0]);
         BufferedReader br = new BufferedReader(new FileReader(file));
@@ -24,13 +52,10 @@ public class PathSumFourWays {
 
         int i_max = arr.size() - 1;
         int j_max = arr.get(0).size() - 1;
-        // its a graph with nodes
         Node[][] graph = new Node[i_max + 1][j_max + 1];
 
-        //issa a queue
         PriorityQueue<Node> q = new PriorityQueue<>();
 
-        // fill in the graph with nodes and also begin dijkstras thing
         for (int i = 0; i <= i_max; i++){
             for (int j = 0; j <= j_max; j++) {
                 graph[i][j] = new Node(arr.get(i).get(j), i, j);
@@ -40,15 +65,16 @@ public class PathSumFourWays {
         graph[0][0].dist = graph[0][0].weight;
         
         while (!q.isEmpty()) {
-            //grabs and removes the head of the queue
             Node u = q.poll();
             System.out.println(u.weight + " " + u.dist);
 
-            if (u.weight == graph[i_max][j_max].weight) {
+            if (u == graph[i_max][j_max]) {
                 // Backtrack
+
+
                 break;
             } else {
-                if (u.i != 0)  { // Get node above
+                if (u.i != 0)  { // Up
                     int up = graph[u.i - 1][u.j].weight + u.dist;
                     if (up < graph[u.i - 1][u.j].dist) {
                         q.remove(graph[u.i - 1][u.j]);
@@ -56,7 +82,7 @@ public class PathSumFourWays {
                         q.add(graph[u.i - 1][u.j]);
                     }
                 }
-                if (u.i != i_max) { // the down case
+                if (u.i != i_max) { // Down
                     int down = graph[u.i + 1][u.j].weight + u.dist;
                     if (down < graph[u.i + 1][u.j].dist) {
                         q.remove(graph[u.i + 1][u.j]);
@@ -64,7 +90,7 @@ public class PathSumFourWays {
                         q.add(graph[u.i + 1][u.j]);
                     }
                 }
-                if (u.j != 0) { // the left case
+                if (u.j != 0) { // Left
                     int left = graph[u.i][u.j - 1].weight + u.dist;
                     if (left < graph[u.i][u.j - 1].dist) {
                         q.remove(graph[u.i][u.j - 1]);
@@ -72,7 +98,7 @@ public class PathSumFourWays {
                         q.add(graph[u.i][u.j - 1]);
                     }
                 }
-                if (u.j != j_max) { // the right case
+                if (u.j != j_max) { // Right
                     int right = graph[u.i][u.j + 1].weight + u.dist;
                     if (right < graph[u.i][u.j + 1].dist) {
                         q.remove(graph[u.i][u.j + 1]);
@@ -86,34 +112,3 @@ public class PathSumFourWays {
     }
 }
 
-// node class
-class Node implements Comparable<Node>{
-        
-    int weight;
-    
-    int dist = Integer.MAX_VALUE;
-
-    int i;
-    int j;
-
-    Node(int weight, int i, int j) {
-        this.weight = weight;
-        this.i = i;
-        this.j = j;
-    }
-
-    
-    public int compare(Node n1, Node n2) {
-        if (n1.dist == n2.dist) return 0;
-        //returns 1 if greater
-        if (n1.dist > n2.dist) return 1;
-        return -1;
-    } 
-
-    public int compareTo(Node comp) {
-        if (this.dist == comp.dist) return 0;
-        if (this.dist > comp.dist) return 1;
-        return -1;
-    }
-
-}
